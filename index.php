6b7b399cfa27ff1d41a610b72a5a09d0852fc131
Jake Jarvis <jakejarvis@gmail.com>
2012-06-11 13:32:26 -0400

    refresh 30 secs

--- index.php
+++ index.php
@@ -23,8 +23,8 @@
   <script>window.jQuery || document.write(unescape('%3Cscript src="<?php echo STATIC_ASSETS_URL ?>/js/jquery.min.js"%3E%3C/script%3E'))</script>
   <script>
     setInterval(function() {
-      $('#friends').load('friends.php', {'signed_request':'<?php echo $_POST['signed_request'] ?>'}).fadeIn('fast');
-    }, 10000);
+      $('#friends').load('friends.php', {'signed_request':'<?php echo $_POST['signed_request'] ?>'}).fadeIn('slow');
+    }, 30000);
   </script>
   <script src="//connect.facebook.net/en_US/all.js"></script>
   <script>
