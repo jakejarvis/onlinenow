c29e662a2e3d3cc7383fce85920e17cd47a096be
Jake Jarvis <jakejarvis@gmail.com>
2012-06-10 23:13:01 -0400

    move all JS to the bottom

--- index.php
+++ index.php
@@ -48,15 +48,6 @@
 </head>
 <body>
   <div id="fb-root"></div>
-  <script src="//connect.facebook.net/en_US/all.js"></script>
-  <script>
-    FB.init({
-      appId      : '<?php echo $app_id ?>',
-      channelUrl : '<?php echo $callback_url ?>/channel.php',
-      xfbml      : false,
-    });
-    FB.Canvas.setAutoGrow(1000);
-  </script>
   <?php echo ad() ?>
   <div id="wrapper">
     <div id="header">
@@ -97,13 +88,22 @@
   }
 ?>  </div>
   <?php echo ad() ?>
+  <script src="//connect.facebook.net/en_US/all.js"></script>
   <script>
+    FB.init({
+      appId      : '<?php echo $app_id ?>',
+      channelUrl : '<?php echo $callback_url ?>/channel.php',
+      xfbml      : false,
+    });
+    FB.Canvas.setAutoGrow(1000);
+
     var _gaq = [['_setAccount', 'UA-1563964-18'], ['_setDomainName', 'none'], ['_setAllowLinker', true], ['_trackPageview']];
     (function() {
       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
     })();
+
     var _gauges = _gauges || [];
     (function() {
       var t   = document.createElement('script');
