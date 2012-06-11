64ec3fba2e18b87f528b7669889c3a50efaf35a9
Jake Jarvis <jakejarvis@gmail.com>
2012-06-10 23:17:53 -0400

    js cleanup

--- index.php
+++ index.php
@@ -90,20 +90,16 @@
   <?php echo ad() ?>
   <script src="//connect.facebook.net/en_US/all.js"></script>
   <script>
+    // autogrow the canvas
     FB.init({
       appId      : '<?php echo $app_id ?>',
       channelUrl : '<?php echo $callback_url ?>/channel.php',
+      status     : false,
+      cookie     : false,
       xfbml      : false,
     });
     FB.Canvas.setAutoGrow(1000);
-
-    var _gaq = [['_setAccount', 'UA-1563964-18'], ['_setDomainName', 'none'], ['_setAllowLinker', true], ['_trackPageview']];
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
+    // gaug.es tracking code <3
     var _gauges = _gauges || [];
     (function() {
       var t   = document.createElement('script');
@@ -114,6 +110,13 @@
       t.src = '//secure.gaug.es/track.js';
       var s = document.getElementsByTagName('script')[0];
       s.parentNode.insertBefore(t, s);
+    })();
+    // google analytics
+    var _gaq = [['_setAccount', 'UA-1563964-18'], ['_setDomainName', 'none'], ['_setAllowLinker', true], ['_trackPageview']];
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
     })();
   </script>
 </body>
